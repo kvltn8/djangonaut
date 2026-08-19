63a22a4fd6654f262bd98f4654b1b66587d22e8e
Kaltun <kaltunabdullahi070@gmail.com>
2026-08-20 00:13:34 +0300

    Testing validation logic success

--- backend/kaltunsAbayaShop/views.py
+++ backend/kaltunsAbayaShop/views.py
@@ -4,5 +4,5 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def server(request):
-  raise Exception("Intentionally breaking the server")
+  return Response("Server is running", status=status.HTTP_200_OK)
  
