9f7d34f767ac70c7cfea7f0f9258a4d1a21e5fcf
Kaltun <kaltunabdullahi070@gmail.com>
2026-08-23 00:46:39 +0300

    fixed the allowed hosts

--- backend/kaltunsAbayaShop/settings.py
+++ backend/kaltunsAbayaShop/settings.py
@@ -30,7 +30,7 @@ DATABASE_URL=os.getenv("DATABASE_URL")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost, 127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv( "localhost", "127.0.0.1").split(",")
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
 
