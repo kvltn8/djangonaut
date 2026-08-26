89a567c1a6d26579de4e80d26b53db3b0419a159
Kaltun <kaltunabdullahi070@gmail.com>
2026-08-26 23:47:58 +0300

    syntax error fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ echo "Running migrations......"
 python manage.py migrate --noinput
 
 echo "collecting static files....."
-python manage.py collect static
+python manage.py collectstatic --noinput
 
 echo "starting gunicorn......"
 exec gunicorn kaltunsAbayaShop.wsgi:application --bind 0.0.0.0:$PORT
