76b34fa3403f839b1a3e6350c7637dac039ec194
Kaltun <kaltunabdullahi070@gmail.com>
2026-09-01 23:37:34 +0300

    fix

--- backend/kaltunsAbayaShop/settings.py
+++ backend/kaltunsAbayaShop/settings.py
@@ -157,7 +157,7 @@ STORAGES = {
     },
 }
 MEDIA_URL= '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'storefront.media')
+MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
