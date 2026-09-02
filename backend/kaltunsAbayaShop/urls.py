61f6221ae42c5161c9811348a3ace230e095c9b6
Kaltun <kaltunabdullahi070@gmail.com>
2026-09-03 01:10:09 +0300

    final clean up

--- backend/kaltunsAbayaShop/urls.py
+++ backend/kaltunsAbayaShop/urls.py
@@ -32,14 +32,6 @@ urlpatterns = [
     path('', include('storefront.urls'))
 ]
 
-#if settings.DEBUG: 
-  #urlpatterns + static(
-    #settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
- # )
 
 
-if not  settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
+
--- backend/storefront/storage.py
+++ backend/storefront/storage.py
@@ -5,7 +5,9 @@ from django.conf import settings
 supabase = create_client(settings.SUPABASE_URL, settings.SUPABASE_SERVICE_KEY)#creating table in supabase.
 
 def uploadproductimage(file):
-  extension=file.name.split(".")
+  extension = file.name.split(".")[-1]
   filename = f"{uuid.uuid4()}.{extension}"
-  supabase.storage.from_("productimages").upload(filename.file.read(),{"content-type":file.content_type})
+  supabase.storage.from_("productimages").upload(
+      filename, file.read(), {"content-type": file.content_type}
+  )
   return supabase.storage.from_("productimages").get_public_url(filename)
